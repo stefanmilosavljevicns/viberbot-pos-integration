2d0dd61e051680b8d17212df01b2f1dbe5f5eb7a
stefanmilosavljevicns <82588835+stefanmilosavljevicns@users.noreply.github.com>
2023-01-08 23:14:46 +0100

    Update UsersRepository.java

--- src/main/java/com/payten/FoodRest/repository/UsersRepository.java
+++ src/main/java/com/payten/FoodRest/repository/UsersRepository.java
@@ -4,4 +4,9 @@ import com.payten.FoodRest.model.Users;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UsersRepository extends MongoRepository<Users, String> {
+    @Aggregation(pipeline = { "{ '$group': { '_id' : '$location' } }" })
+    List<String> findDistinctLocations();
+
+    @Query(fields = "{'location': 0}")
+    List<Users> findByLocation(String location);
 }
