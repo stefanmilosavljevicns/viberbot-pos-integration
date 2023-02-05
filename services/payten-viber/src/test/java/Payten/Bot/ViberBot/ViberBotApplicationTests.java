4c770e0515110b857430f7bcf3a1edded7e87a42
stefanmilosavljevicns <82588835+stefanmilosavljevicns@users.noreply.github.com>
2023-02-05 12:49:41 +0100

    reorganizing project

---
+++ services/payten-viber/src/test/java/Payten/Bot/ViberBot/ViberBotApplicationTests.java
@@ -0,0 +1,13 @@
+package Payten.Bot.ViberBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViberBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
