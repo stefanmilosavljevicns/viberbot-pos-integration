60ad4a696f823184c370c94c77cdf050c9b8f9b0
stefanmilosavljevicns <82588835+stefanmilosavljevicns@users.noreply.github.com>
2023-04-09 03:55:26 +0200

    fix

--- services/client/app.py
+++ services/client/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__,template_folder='templates')
 @app.route('/paymentinfo',methods=['POST'])
 def index():
     viberId = request.args.get('viberId')
-    viberPath = "https://sputnik-it.rs"+"/"+request.args.get('viberPath')+"/external-paying"
+    viberPath = "https://sputnik-it.rs"+request.args.get('viberPath')+"/external-paying"
     return render_template('index.html',viberId=viberId,viberPath=viberPath)
 
 if __name__ == "__main__":    
