4536c315c4ea0a2df515bacc583a754c57e0f9e0
stefanmilosavljevicns <82588835+stefanmilosavljevicns@users.noreply.github.com>
2023-04-20 06:40:19 +0200

    fix

--- services/client/app.py
+++ services/client/app.py
@@ -1,18 +1,27 @@
-from flask import Flask,request,render_template, abort
+from flask import Flask,request,render_template, abort,url_for, redirect
 
 app = Flask(__name__,template_folder='templates')
 
-@app.route('/paymentinfo',methods=['POST'])
+@app.route('/paymentinfo', methods=['POST'])
 def index():
     referer = request.headers.get('referer')
-    print("BODY:"+request.data.decode('utf-8'))
-    app.logger.warning('BODY: %s', request.data.decode('utf-8'))
-    app.logger.warning('Invalid referer: %s', referer)    
     if referer != 'https://entegrasyon.asseco-see.com.tr/':
-        abort(403)  # Return a 403 Forbidden error if the referer is not the expected URL    
+        abort(403)
+    
+    # process payment here
+    # ...
+    
     viberId = request.args.get('viberId')
     viberPath = "https://sputnik-it.rs"+request.args.get('viberPath')+"/external-paying?viberId="+viberId
-    return render_template('index.html',viberPath=viberPath)
+    
+    return redirect(url_for('payment_success', viberPath=viberPath))
+
+@app.route('/payment_success', methods=['GET'])
+def payment_success():
+    viberPath = request.args.get('viberPath')
+    return render_template('success.html', viberPath=viberPath)
+
+
 
 if __name__ == "__main__":    
     app.run(host="0.0.0.0", port=5000, debug=True)
