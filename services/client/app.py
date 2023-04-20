9cc30fa123f92ed5f93bd9464b5a612831e04aa7
stefanmilosavljevicns <82588835+stefanmilosavljevicns@users.noreply.github.com>
2023-04-20 06:16:34 +0200

    fix

--- services/client/app.py
+++ services/client/app.py
@@ -6,7 +6,8 @@ app = Flask(__name__,template_folder='templates')
 def index():
     referer = request.headers.get('referer')
     print("BODY:"+request.data.decode('utf-8'))
-    print("REFERER:"+referer)
+    app.logger.warning('BODY: %s', request.data.decode('utf-8'))
+    app.logger.warning('Invalid referer: %s', referer)    
     if referer != 'https://entegrasyon.asseco-see.com.tr/chipcard/pay3d/':
         abort(403)  # Return a 403 Forbidden error if the referer is not the expected URL    
     viberId = request.args.get('viberId')
