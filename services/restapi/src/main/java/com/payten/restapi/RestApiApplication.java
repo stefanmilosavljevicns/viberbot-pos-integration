8f19c1ba1a2210214787260b9601010f68344522
Stefan Milosavljević <stefanmilosavljevicns@gmail.com>
2023-10-20 21:03:48 +0200

    rename

--- services/restapi/src/main/java/com/payten/restapi/RestApiApplication.java
+++ services/restapi/src/main/java/com/payten/restapi/RestApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class restapiApplication {
+public class RestApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(restapiApplication.class, args);
+		SpringApplication.run(RestApiApplication.class, args);
 	}
 
 }
