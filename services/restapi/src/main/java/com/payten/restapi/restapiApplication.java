f31b9ade2a416cc0ab20a969183db94e2c1c6787
Stefan Milosavljević <stefanmilosavljevicns@gmail.com>
2023-10-22 00:23:38 +0200

    fix

--- services/restapi/src/main/java/com/payten/restapi/restapiApplication.java
+++ services/restapi/src/main/java/com/payten/restapi/restapiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestApiApplication {
+public class restapiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestApiApplication.class, args);
+		SpringApplication.run(restapiApplication.class, args);
 	}
 
 }
