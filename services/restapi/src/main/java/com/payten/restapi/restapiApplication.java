da5155f66b155b10bcb88883e60df6c097dabd43
stefanmilosavljevicns <82588835+stefanmilosavljevicns@users.noreply.github.com>
2023-10-23 19:12:57 +0200

    refactor

--- services/restapi/src/main/java/com/payten/restapi/restapiApplication.java
+++ services/restapi/src/main/java/com/payten/restapi/restapiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class restapiApplication {
+public class RestapiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(restapiApplication.class, args);
+		SpringApplication.run(RestapiApplication.class, args);
 	}
 
 }
