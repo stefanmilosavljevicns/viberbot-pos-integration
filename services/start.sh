c2adc39c18037d74735b399466fcf1208f2135eb
stefanmilosavljevicns <82588835+stefanmilosavljevicns@users.noreply.github.com>
2023-11-07 19:21:16 +0100

    move-to-prod

--- services/start.sh
+++ services/start.sh
@@ -10,23 +10,18 @@ echo '~~~~~~ 1. BULDING REST IMAGE ~~~~~~'
 docker build ./restapi -t restapi
 echo '~~~~~~ 2. BULDING VIBER-BOT IMAGE ~~~~~~'
 docker build ./bot -t bot
-
 echo '~~~~~~ CREATING DOCKER NETWORK ~~~~~~'
 echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-docker network create nginx-net
+docker network create -d overlay nginx-net
 echo '~~~~~~ STARTING DOCKER SERVICES ~~~~~~'
 echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 echo '~~~~~~ 1. STARTING NGINX ~~~~~~'
-docker-compose -f ./nginx/docker-compose.yml down
-docker-compose -f ./nginx/docker-compose.yml up -d
-sleep 15
+docker stack deploy --compose-file ./nginx/docker-compose.yml nginx
+sleep 5
 echo '~~~~~~ 2. STARTING REST ~~~~~~'
-docker-compose -f ./restapi/docker-compose.yml down
-docker-compose -f ./restapi/docker-compose.yml up -d
-sleep 15
+docker stack deploy --compose-file ./restapi/docker-compose.yml rest
+sleep 5
 echo '~~~~~~ 3. STARTING VIBER-BOT ~~~~~~'
-docker-compose -f ./bot/docker-compose.yml down
-docker-compose -f ./bot/docker-compose.yml up -d
-sleep 15
+docker stack deploy --compose-file ./bot/docker-compose.yml down
