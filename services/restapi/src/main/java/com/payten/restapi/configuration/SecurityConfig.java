152b06bab0d5a8bd4caf9af56dba219225485ddc
stefanmilosavljevicns <82588835+stefanmilosavljevicns@users.noreply.github.com>
2023-12-16 17:00:02 +0100

    loggingsec

--- services/restapi/src/main/java/com/payten/restapi/configuration/SecurityConfig.java
+++ services/restapi/src/main/java/com/payten/restapi/configuration/SecurityConfig.java
@@ -19,7 +19,8 @@ public class SecurityConfig {
                         .requestMatchers(request -> "localhost".equals(request.getServerName())).permitAll()
                         .requestMatchers(request -> "ws".contains(request.getScheme())).permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
         return http.build();
     }
 
