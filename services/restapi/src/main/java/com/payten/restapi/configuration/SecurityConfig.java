ebf618ef53dd2768f702049b4b2e1581687eb8fd
stefanmilosavljevicns <82588835+stefanmilosavljevicns@users.noreply.github.com>
2023-12-16 15:30:05 +0100

    remoteaddr

--- services/restapi/src/main/java/com/payten/restapi/configuration/SecurityConfig.java
+++ services/restapi/src/main/java/com/payten/restapi/configuration/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(request -> "10.0.1".contains(request.getLocalAddr())).permitAll()
+                        .requestMatchers(request -> "10.0.1".contains(request.getRemoteAddr())).permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
