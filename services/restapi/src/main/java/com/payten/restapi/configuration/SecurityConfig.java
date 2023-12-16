841e209c3ecd1f2b7731e5cb1c4e7c811cdd6a29
stefanmilosavljevicns <82588835+stefanmilosavljevicns@users.noreply.github.com>
2023-12-16 16:54:23 +0100

    removingouath

--- services/restapi/src/main/java/com/payten/restapi/configuration/SecurityConfig.java
+++ services/restapi/src/main/java/com/payten/restapi/configuration/SecurityConfig.java
@@ -19,8 +19,7 @@ public class SecurityConfig {
                         .requestMatchers(request -> "localhost".equals(request.getServerName())).permitAll()
                         .requestMatchers(request -> "ws".contains(request.getScheme())).permitAll()
                         .anyRequest().authenticated()
-                )
-                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+                );
         return http.build();
     }
 
