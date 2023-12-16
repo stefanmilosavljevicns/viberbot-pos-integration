d5d1dfea29c242feaae941f240996ec014e4a5a6
Mladjenovic <mladjenovic.cvijetin@gmail.com>
2023-12-16 13:06:09 +0100

    Updated security config

--- services/restapi/src/main/java/com/payten/restapi/configuration/SecurityConfig.java
+++ services/restapi/src/main/java/com/payten/restapi/configuration/SecurityConfig.java
@@ -15,8 +15,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/v1/getallCategories").authenticated()
-                        .anyRequest().permitAll()
+                        .requestMatchers("127.0.0.1").permitAll()
+                        .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
         return http.build();
