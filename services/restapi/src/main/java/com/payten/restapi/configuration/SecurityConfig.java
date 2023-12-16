9e9361e0282768f96270cc4cabada341741c2957
stefanmilosavljevicns <82588835+stefanmilosavljevicns@users.noreply.github.com>
2023-12-16 15:03:41 +0100

    remotehost

--- services/restapi/src/main/java/com/payten/restapi/configuration/SecurityConfig.java
+++ services/restapi/src/main/java/com/payten/restapi/configuration/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(request -> "viber-bot".contains(request.getRemoteAddr()))
+                        .requestMatchers(request -> "viber-bot".contains(request.getRemoteHost()))
                         .permitAll()
                         .anyRequest()
                         .authenticated()
