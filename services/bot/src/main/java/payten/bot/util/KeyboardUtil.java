a7e647ffbf13d73677b1f85349c7bf5992aad851
stefanmilosavljevicns <82588835+stefanmilosavljevicns@users.noreply.github.com>
2023-12-27 22:22:29 +0100

    more-style

--- services/bot/src/main/java/payten/bot/util/KeyboardUtil.java
+++ services/bot/src/main/java/payten/bot/util/KeyboardUtil.java
@@ -134,7 +134,7 @@ public class KeyboardUtil {
         mainMenu.setInputFieldState("hidden");
         mainMenu.setType("keyboard");
         mainMenu.addButton(new ViberButton(selectDayReservation)
-                        .setText("Rezerviši")
+                        .setText("<font color='#ffffff'><b>Rezerviši</b></font>")
                         .setImage(stringUtils.getIconReserve())
                         .setTextSize(ViberButton.TextSize.LARGE)
                         .setTextHAlign(ViberButton.TextAlign.MIDDLE)
@@ -144,7 +144,7 @@ public class KeyboardUtil {
                         .setSilent(true)
                         .setRows(2));
         mainMenu.addButton(new ViberButton(startReservationProcess)
-                        .setText("O nama")
+                        .setText("<font color='#ffffff'><b>O nama</b></font>")
                         .setImage(stringUtils.getIconAboutUs())
                         .setTextSize(ViberButton.TextSize.LARGE)
                         .setBgColor(stringUtils.getSecondarilyColor())
@@ -154,7 +154,7 @@ public class KeyboardUtil {
                         .setSilent(true)
                         .setRows(2));
         mainMenu.addButton(new ViberButton(startReservationProcess)
-                        .setText("Izaberite jezik")
+                        .setText("<font color='#ffffff'><b>Izaberite jezik</b></font>")
                         .setImage(stringUtils.getIconChooseLanguage())
                         .setTextSize(ViberButton.TextSize.LARGE)
                         .setTextHAlign(ViberButton.TextAlign.MIDDLE)
@@ -165,7 +165,7 @@ public class KeyboardUtil {
                         .setSilent(true)
                         .setRows(2));
         mainMenu.addButton(new ViberButton(startReservationProcess)
-                        .setText("Istorija rezervacija")
+                        .setText("<font color='#ffffff'><b>Istorija rezervacija</b></font>")
                         .setTextSize(ViberButton.TextSize.LARGE)
                         .setTextHAlign(ViberButton.TextAlign.MIDDLE)
                         .setTextVAlign(ViberButton.TextAlign.BOTTOM)
