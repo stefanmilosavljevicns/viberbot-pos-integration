aea075b2a75cdd98c1f0a753db1f0330d74aa725
stefanmilosavljevicns <82588835+stefanmilosavljevicns@users.noreply.github.com>
2023-12-29 01:21:42 +0100

    fix

--- services/bot/src/main/java/payten/bot/configuration/LocaleConfig.java
+++ services/bot/src/main/java/payten/bot/configuration/LocaleConfig.java
@@ -3,12 +3,13 @@ package payten.bot.configuration;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class LocaleConfig {
+public class LocaleConfig implements WebMvcConfigurer {
     @Bean
     public MessageSource messageSource() {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("messages");
+        messageSource.setBasename("languages/messages");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
     }
